6c622cb015dc8d9a635a2a310edf98b7d2d0d15a
Victor Oseji <vctroseji@gmail.com>
2026-03-03 05:52:07 +0100

    update

--- routes/api.php
+++ routes/api.php
@@ -183,7 +183,7 @@ Route::middleware(['auth.jwt'])->group(function () {
     | ADMIN ONLY – PARTNERS / SUPPLIERS
     |--------------------------------------------------------------------------
     */
-    Route::middleware('role:PATIENT,SUPER_ADMIN,CHAI')->group(function () {
+    Route::middleware('role:PATIENT,SUPER_ADMIN,CHAI,DOCTOR')->group(function () {
         Route::post('/patient/biodata', [PatientBiodataController::class, 'store']);
         Route::get('/patients/biodata/{phoneNumber}', [PatientBiodataController::class, 'retrievePatient']);
         Route::get('patient/{phoneNumber}/status', [PatientBiodataController::class, 'currentStatus']);
